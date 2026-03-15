ce1365bb79fde7dbef12b09c8ce116fe85027ba3
Fatima Karash <lkitto77@gmail.com>
2026-03-16 02:45:06 +0600

    feat: add cube calculation function

--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import "fmt"
 
 func main() {
 	number := 7
+	result := square(number)
+	fmt.Println(result)
 }
 func square(n int) int {
 	return n * n
+}
+func cube(n int) int {
+	return n * n * n
 }
